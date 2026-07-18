f3f816d3190d8d4ff04330f1f903a7c3df36a1d1
Aletoxic <Alexandregalvie@gmail.com>
2026-07-18 10:48:07 -0300

    Definição das funções criar Heroi/criar inimigo/ mostrarficha/ descansar/ poder total

--- game.js
+++ game.js
@@ -1,66 +1,92 @@
-const jogador = {
-    nome: "Alexandre",
-    Vida: 100,
-    Vida_maxima: 1000,
-    Defesa: 10,
-    Ataque: 20,
-    Xp: 0,
-    Ouro: 0,
-    Esta_vivo: true,
-    classe: "Paladino",
-    Xp_maximo: 1000,
-    Arma: "Espada reta",
-};
+function criarHeroi(nome, classe) {
+    return {
+        nome: nome,
+        classe: classe,
+        vida: 1000,
+        vidaMaxima: 1000,
+        defesa: 10,
+        ataque: 20,
+        xp: 0,
+        ouro: 0,
+        estaVivo() {
+            return this.vida > 0;
+        },
+        xpMaximo: 1000,
+        inventário: [],
+        nivel: 1,
+    }
+}
 
-console.log('\n===Ficha de atributos do jogador 1:===')
-console.log(`O nome do jogador 1 é: ${jogador.nome}`)
-console.log(`A vida do jogador 1 é: ${jogador.Vida}`)
-console.log(`A vida máxima do jogador 1 é: ${jogador.Vida_maxima}`)
-console.log(`A defesa do jogador 1 é: ${jogador.Defesa}`)
-console.log(`O ataque do jogador 1 é: ${jogador.Ataque}`)
-console.log(`O xp do jogador 1 é: ${jogador.Xp}`)
-console.log(`O ouro do jogador 1 é: ${jogador.Ouro}`)
-console.log(`O jogador 1 está vivo? ${jogador.Esta_vivo}`)
-console.log(`A classe do jogador 1 é: ${jogador.classe}`)
-console.log(`O xp máximo do jogador 1 é: ${jogador.Xp_maximo}\n`)
-console.log(`A arma do jogador 1 é: ${jogador.Arma}\n`)
+function criarInimigo(nome, classe, vida, vidaMaxima, defesa, ataque, ouro, xpMaximo) {
+    return {
+        nome: nome, 
+        classe: classe,
+        vida: vida,
+        vidaMaxima: vidaMaxima,
+        defesa:defesa,
+        ataque: ataque,
+        estaVivo(){
+            return this.vida > 0;
+        },
+        ouro: ouro,
+        xpMaximo: xpMaximo,
+    }
+}
 
+function mostrarficha(mob){
+    console.log('\n===Ficha de atributos do ' + mob.nome + ':===')
+    console.log('O nome do jogador é: ' + mob.nome)
+    console.log('A vida do jogador é: ' + mob.vida)
+    console.log('A vida máxima do jogador é: ' + mob.vidaMaxima)
+    console.log('A defesa do jogador é: ' + mob.defesa)
+    console.log('O ataque do jogador é: ' + mob.ataque)
+    console.log('O xp do jogador é: ' + mob.xpMaximo)
+    console.log('O ouro do jogador é: ' + mob.ouro)
+    console.log('O jogador está vivo? ' + mob.estaVivo())
+    console.log('A classe do jogador é: ' + mob.classe)
+    console.log('O xp máximo do jogador é: ' + mob.xpMaximo)
+}
 
-const inimigo = {
-    nome: "Morto-vivo",
-    vida: 20,
-    defesa: 5,
-    ataque: 15,
-    xp_ganho: 50,
-    ouro_ganho: 1,
-};
+function descansar(jogador) {
+    if(jogador.vida === jogador.vidaMaxima){
+        console.log('O heroi insite que pode continuar a lutar, sem descansar')
+    }
+    else{
+        let vida_inicial = jogador.vida;
+        jogador.vida += 30;
+            if (jogador.vida > jogador.vidaMaxima) {
+                jogador.vida = jogador.vidaMaxima;
+            }
+            let vida_recuperada = jogador.vida - vida_inicial;
+            console.log('O heroi reculperou suas energias e recuperou ' + vida_recuperada + ' de vida')
+        }
+}
 
-// === Definindo a força do usuário=== //
-let forcaTotal = heroi.ataque + heroi.nivel * 2;
-console.log(`Força total do herói: ${forcaTotal}`);
+function poder_total(jogador){ 
+    forcaTotal = jogador.ataque + jogador.defesa + jogador.nivel * 2;
+    console.log('A força total do heroi é: ' + forcaTotal)
+}
 
-// === Definindo dano ao usuário por armadilha e/ou ataque === //
-let dano = 15;
-heroi.vida = heroi.vida - dano;
-console.log(`Armadilha! O herói perdeu ${dano} de vida.`);
-console.log(`Vida atual: ${heroi.vida}`);
-heroi.vida -= 10;   
-console.log(`Levou outro golpe. Vida atual em: ${heroi.vida}`);
+const jogador = criarHeroi("Alexandre", "Mago")
+const inimigo = criarInimigo("Goblin", "Monstro", 20, 20, 5, 10, 1, 10)
 
-// === Definindo a cura dos status de vida do usuário === //
-heroi.vida += 30;
-console.log(`Depois de descansar, vida: ${heroi.vida}`);
-let percentual = (heroi.vida / heroi.vidaMaxima) * 100;
-console.log(`Vida: ${percentual}% do total`);
+mostrarficha(inimigo)
+mostrarficha(jogador)
+descansar(jogador)
+poder_total(jogador)
 
-// === Definindo o poder do heroi === //
-let poder = heroi.ataque * 2 + heroi.defesa + heroi.nivel * 5;
-console.log(`Poder de combate: ${poder}`);
+// === Definindo dano ao usuário por armadilha e/ou ataque === 
+//let dano = 15;
+//jogador.vida -= dano;
+//console.log(`Armadilha! O herói perdeu ${dano} de vida.`);
+//console.log(`Vida atual: ${heroi.vida}`);
+//heroi.vida -= 10;   
+//console.log(`Levou outro golpe. Vida atual em: ${heroi.vida}`);
 
-console.log('\n===Ficha de atributos do inimigo:===')
-console.log(`O nome do inimigo é: ${inimigo.nome}`)
-console.log(`A vida do inimigo é: ${inimigo.vida}`)
-console.log(`A defesa do inimigo é: ${inimigo.defesa}`)
-console.log(`O ataque do inimigo é: ${inimigo.ataque}`)
-console.log(`O xp ganho do inimigo é: ${inimigo.xp_ganho}`)
-console.log(`O ouro ganho do inimigo é: ${inimigo.ouro_ganho}\n`)
+//Aqui os comandos,em sequência, para enviar o código para o GitHub:
+// git add .
+// git commit -m ""
+// git push origin main(o main é por conta do branch que está sendo utilizado)
+
+//Para execultar um arquivo JavaScript no terminal, utilize o comando: node nome_do_arquivo.js
+//Lembrando que tem que estar no diretório do arquivo para executar o comando acima.
