5cc07604d1edd52e1e152f630c2589a9fb3bc11a
Aletoxic <Alexandregalvie@gmail.com>
2026-08-10 14:24:33 -0300

    Adição de comparação de força, redefinição de atibutos de status e correção das funções de força_total, definir nível e descandar

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
-function criarHeroi(nome, classe) {
+function criarHeroi(nome, classe, vida, vidaMaxima, defesa, ataque, nivel, xp, ouro, xpMaximo) {
     return {
         nome: nome,
         classe: classe,
-        vida: 1000,
-        vidaMaxima: 1000,
-        defesa: 10,
-        ataque: 20,
-        xp: 0,
-        ouro: 0,
+        vida: vida,
+        vidaMaxima: vidaMaxima,
+        defesa: defesa,
+        ataque: ataque,
+        nivel: nivel,        
+        xp: xp,
+        ouro: ouro,
+        xpMaximo: xpMaximo,
+        inventário: [],
         estaVivo() {
             return this.vida > 0;
-        },
-        xpMaximo: 1000,
-        inventário: [],
-        nivel: 1,
+        }
     }
 }
 
-function criarInimigo(nome, classe, vida, vidaMaxima, defesa, ataque, ouro, xpMaximo) {
+function criarInimigo(nome, classe, vida, vidaMaxima, defesa, ataque, nivel, xp, ouro, xpMaximo) {
     return {
         nome: nome, 
         classe: classe,
@@ -25,64 +25,84 @@ function criarInimigo(nome, classe, vida, vidaMaxima, defesa, ataque, ouro, xpMa
         vidaMaxima: vidaMaxima,
         defesa:defesa,
         ataque: ataque,
+        nivel: nivel,
+        xp: xp,
+        ouro: ouro,
+        xpMaximo: xpMaximo,
         estaVivo(){
             return this.vida > 0;
-        },
-        ouro: ouro,
-        xpMaximo: xpMaximo
+        }
     }
 }
 
 function mostrarficha(mob){
     console.log('\n===Ficha de atributos do ' + mob.nome + ':===')
-    console.log('O nome do mob: ' + mob.nome)
-    console.log('A vida do mob é: ' + mob.vida)
-    console.log('A vida máxima do mob é: ' + mob.vidaMaxima)
-    console.log('A defesa do mob é: ' + mob.defesa)
-    console.log('O ataque do mob é: ' + mob.ataque)
-    console.log('O xp do mob é: ' + mob.xp)
-    console.log('O ouro do mob é: ' + mob.ouro)
-    console.log('O mob está vivo? ' + mob.estaVivo())
-    console.log('A classe do mob é: ' + mob.classe)
-    console.log('O xp máximo do mob é: ' + mob.xpMaximo)
+    console.log('O nome do ' + mob.classe + ' é: ' + mob.nome)
+    console.log('A vida do ' + mob.classe + ' é: ' + mob.vida)
+    console.log('A vida máxima do ' + mob.classe + ' é: ' + mob.vidaMaxima)
+    console.log('A defesa do ' + mob.classe + ' é: ' + mob.defesa)
+    console.log('O ataque do ' + mob.classe + ' é: ' + mob.ataque)
+    console.log('A Força total do ' + mob.classe + ' é: ' + força_total(mob))
+    console.log('O nível do ' + mob.classe + ' é: ' + mob.nivel)
+    console.log('O xp do ' + mob.classe + ' é: ' + mob.xp)
+    console.log('O ouro do ' + mob.classe + ' é: ' + mob.ouro)
+    console.log('O ' + mob.classe + ' está vivo? ' + mob.estaVivo())
+    console.log('O xp máximo do ' + mob.classe + ' é: ' + mob.xpMaximo)
 }
 
 function descansar(jogador) {
     if(jogador.vida === jogador.vidaMaxima){
-        console.log('O heroi insite que pode continuar a lutar, sem descansar')
-    }
-    if(jogador.vida <= 100){
-        console.log('O heroi está gravimente debilitado e não consegue descansar, dado o seu estado, ele não consegue se reculperar')
-    }
-    else{
+        console.log('O heroi insite que pode continuar a lutar, sem descansar.')
+    }else if(jogador.vida < 10){
+        console.log('O heroi está gravimente debilitado e não consegue descansar, dado o seu estado, ele não consegue se reculperar.')
+    }else{
         let vida_inicial = jogador.vida;
         jogador.vida += 30;
             if (jogador.vida > jogador.vidaMaxima) {
                 jogador.vida = jogador.vidaMaxima;
             }
             let vida_recuperada = jogador.vida - vida_inicial;
-            console.log('O heroi reculperou suas energias e recuperou ' + vida_recuperada + ' de vida')
+            console.log('O heroi reculperou suas energias e recuperou ' + vida_recuperada + ' de vida.')
         }
 }
 
-function força_total(jogador){ 
-    let forcaTotal = jogador.ataque + jogador.defesa + jogador.nivel * 2;
-    console.log('A força total do heroi é: ' + forcaTotal)
+function força_total(mob){ 
+    let forcaTotal = mob.ataque + mob.defesa + mob.nivel * 2;
+    return forcaTotal;
 }
-if (força_total(jogador) > 100) {
-    console.log('${jogador.nome} é um peregrino comum, que está começando a sua jornada')
-} else if(força_total(jogador) > 200) {
-    console.log('${jogador.nome} é um guerreiro formidável, porém a jornada dele está longe de acabar')
-} else {
-    console.log('${jogador.nome} é um heroi em formação')
+
+// === Definindo a progressão do jogador === //
+function definir_nivel(jogador) {
+    let forca = força_total(jogador);
+    if (forca > 200) {
+        console.log(`${jogador.nome} é um guerreiro formidável, porém a jornada dele está longe de acabar.`)
+    }else if(forca > 100) {
+        console.log(`${jogador.nome} é um peregrino comum, que está começando a sua jornada.`)
+    } else {
+    console.log(`${jogador.nome} é um heroi em formação.`)
 }
-const jogador = criarHeroi("Alexandre", "Mago")
-const inimigo = criarInimigo("Goblin", "Monstro", 20, 20, 5, 10, 1, 10)
+}
+// === Comparando a força dos mobs === //
+function compararforça(mob1, mob2) {
+    let forçaTotal1 = força_total(mob1);
+    let forçaTotal2 = força_total(mob2);
+    if (forçaTotal1 > forçaTotal2) {
+        console.log(`${mob1.nome} é mais forte que ${mob2.nome}.`)
+    } else if (forçaTotal1 < forçaTotal2) {
+        console.log(`${mob2.nome} é mais forte que ${mob1.nome}.`)
+    } else {
+        console.log(`${mob1.nome} e ${mob2.nome} possuem a mesma força.`)
+    
+    }
+}// As ordens de cada atributo são: nome, classe, vida, vidaMaxima, defesa, ataque, nivel, xp, ouro, xpMaximo
+const jogador = criarHeroi("Alexandre", "Mago", 100, 100, 10, 20, 1, 0, 0, 1000)
+const inimigo = criarInimigo("Goblin", "Monstro", 20, 20, 5, 10, 1, 10, 100, 1000)
 
 mostrarficha(inimigo)
 mostrarficha(jogador)
 descansar(jogador)
-força_total(jogador)
+compararforça(jogador, inimigo)
+definir_nivel(jogador)
 
 // === Definindo dano ao usuário por armadilha e/ou ataque === 
 //let dano = 15;
